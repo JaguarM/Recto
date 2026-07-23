9995db2b0e50ef1647d910780bd5ab070ac8146d
JaguarM <39597011+JaguarM@users.noreply.github.com>
2026-07-23 19:15:51 +0200

    remove webgl mask

--- webgl_mask/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('webgl/masks', views.generate_masks, name='generate_masks'),
-    path('webgl/mask/<str:doc_hash>/<int:page_num>', views.page_mask, name='page_mask'),
-]
